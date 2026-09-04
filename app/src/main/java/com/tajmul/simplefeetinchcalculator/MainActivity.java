b14319484a974e8f597a970490ecf97e8274f771
tajmulansa <108026880+tajmulansa@users.noreply.github.com>
2026-09-05 00:47:17 +0530

    Update MainActivity.java

--- app/src/main/java/com/tajmul/simplefeetinchcalculator/MainActivity.java
+++ app/src/main/java/com/tajmul/simplefeetinchcalculator/MainActivity.java
@@ -7,9 +7,14 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
 public class MainActivity extends AppCompatActivity {
     
     private WebView webView;
+    private AdView adView;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -17,15 +22,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        webView = findViewById(R.id.webView);
+        // Initialize AdMob
+        MobileAds.initialize(this, initializationStatus -> {});
 
+        // Load Banner Ad
+        adView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+
+        // Setup WebView
+        webView = findViewById(R.id.webView);
         webView.setBackgroundColor(android.graphics.Color.parseColor("#0F0F0F"));
+        
         WebSettings ws = webView.getSettings();
         ws.setJavaScriptEnabled(true);
         ws.setDomStorageEnabled(true);
         ws.setAllowFileAccess(true);
-
         ws.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("file:///android_asset/index.html");
     }
